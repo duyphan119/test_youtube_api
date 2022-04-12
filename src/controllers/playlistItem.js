6ecce0d44048fd25447ead61e0f1d3f076f59f89
duyphan119 <duy.910.2015@gmail.com>
2022-04-12 21:06:33 +0700

    Asd

--- src/controllers/playlistItem.js
+++ src/controllers/playlistItem.js
@@ -1,28 +1,45 @@
-const playListItemService = require('../services/playListItem');
+const playListItemService = require("../services/playListItem");
 const playListItemController = {
-   create: async (req, res) => {
-      const response = await playListItemService.create(req.token, req.cookies.refresh_token, req.body);
-   
-      const { status, data } = response;
-      res.status(status).send(data)
-   },
-   getByPlayListId: async (req, res) => {
-      const response = await playListItemService.getByPlayListId(req.token, req.cookies.refresh_token, req.params.playListId);
-   
-      const { status, data } = response;
-      res.status(status).send(data)
-   },
-   getByVideoId: async (req, res) => {
-      const response = await playListItemService.getByVideoId(req.token, req.cookies.refresh_token, req.params.videoId);
-   
-      const { status, data } = response;
-      res.status(status).send(data)
-   },
-   deletePlayListItem: async (req, res) => {
-      const response = await playListItemService.deletePlayListItem(req.token, req.cookies.refresh_token, req.params.id);
-   
-      const { status, data } = response;
-      res.status(status).send(data)
-   },
-}
+  create: async (req, res) => {
+    const response = await playListItemService.create(
+      req.token,
+      req.cookies.refresh_token,
+      req.body
+    );
+
+    const { status, data } = response;
+    res.status(status).send(data);
+  },
+  getByPlayListId: async (req, res) => {
+    const response = await playListItemService.getByPlayListId(
+      req.token,
+      req.cookies.refresh_token,
+      req.params.playListId
+    );
+
+    const { status, data } = response;
+    res.status(status).send(data);
+  },
+  getByVideoId: async (req, res) => {
+    const response = await playListItemService.getByVideoId(
+      req.token,
+      req.cookies.refresh_token,
+      req.params.videoId
+    );
+
+    const { status, data } = response;
+    res.status(status).send(data);
+  },
+  deletePlayListItem: async (req, res) => {
+    const response = await playListItemService.deletePlayListItem(
+      req.token,
+      req.cookies.refresh_token,
+      req.params.id,
+      req.query.playListId
+    );
+
+    const { status, data } = response;
+    res.status(status).send(data);
+  },
+};
 module.exports = playListItemController;
